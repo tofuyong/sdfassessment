b5e0204c299fc3205859e910840e7ad03531b202
your_name <andreayong@Andreas-MacBook-Air.local>
2023-01-27 12:52:25 +0800

    added method to print top ten highest frequency words

--- task01/src/FrequencyProg.java
+++ task01/src/FrequencyProg.java
@@ -6,12 +6,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 public class FrequencyProg {
-
-    private static final String Map = null;
 
     public void highestFreq (String csvPath) throws IOException, FileNotFoundException {
 
@@ -22,7 +19,7 @@ public class FrequencyProg {
         String line;
         List<String> wordList = new ArrayList<>();
         int count = 0;
-        int maxCount = 0;
+        // int maxCount = 0;
         
         //Read text file from the first line down to EOF, splitting the words and storing in an array 
         while ((line = br.readLine()) != null) {
@@ -35,45 +32,24 @@ public class FrequencyProg {
             }
         }
 
-        //Iterate through individual words in the list 
-        for (int i = 0; i < wordList.size(); i++) {
-            count = 1;
-            for (int j = i + 1; j < wordList.size(); j++) {
-                if (wordList.get(i) == wordList.get(j)) {
-                    count++; 
-                }
-            }
-            if (count > maxCount) {
-                maxCount = count;
-                String hfWord = wordList.get(i);
-                System.out.println("The word that appeared in highest frequency is: " + hfWord);
-            }
-        } 
-
-        //Store words as keys and wordcount as values in a hashmap
+        // Store words as keys and wordcount as values in a hashmap
         Map<String, Integer> wordCount = new HashMap<>();
         for (String word : wordList) {
             if (!wordCount.containsKey(word)){
-                wordCount.put(word, 0);
-            } else {
-                int counter = wordCount.get(word);
-                wordCount.put(word, counter++);
+                wordCount.put(word, 1);
+            } else if (wordCount.containsKey(word)) {
+                count = wordCount.get(word);
+                wordCount.put(word, count+1);
             }
         }
 
-    //     Map<String, Integer> wordcount = new HashMap<>();
-    //    //after assigning keys and values
-    //     wordcount.entrySet()
-    //             .stream()
-    //             .sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
-    //             .limit(10)
-    //             .collect(Collectors.toMap(
-    //                     Map.Entry::getKey,
-    //                     Map.Entry::getValue,
-    //                     (e1, e2) -> e1,
-    //                     LinkedHashMap::new
-    //             )).forEach((s, integer) -> System.out.println(String.format("%s : %s", s, integer)));
-        
+        //Print out top 10 words of highest frequency and their Term-frequency
+        wordCount.entrySet().stream()
+            .sorted((one, another) -> - one.getValue().compareTo(another.getValue())) //sort entries to reverse order  
+            .limit(10) 
+            .forEach(entry -> System.out.println(entry.getKey() + " : " + (float)entry.getValue()/wordList.size()));
+
+        br.close();
     }
 } 
 
