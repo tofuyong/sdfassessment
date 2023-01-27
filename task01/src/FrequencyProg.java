5d7c49263388069d4f5ef9862bc7c6c3c42f96f3
your_name <andreayong@Andreas-MacBook-Air.local>
2023-01-27 11:46:41 +0800

    added method to figure out highest frequency word

--- task01/src/FrequencyProg.java
+++ task01/src/FrequencyProg.java
@@ -4,28 +4,76 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class FrequencyProg {
+
+    private static final String Map = null;
 
     public void highestFreq (String csvPath) throws IOException, FileNotFoundException {
 
         File csvFile = new File(csvPath);
         BufferedReader br = new BufferedReader(new FileReader(csvFile));
 
-        //Initializing variables
+        //Defining variables
         String line;
-        List<String> lineList = new ArrayList<>();
-
-        //Read text file from the first line down to last line and add each line to a list
+        List<String> wordList = new ArrayList<>();
+        int count = 0;
+        int maxCount = 0;
+        
+        //Read text file from the first line down to EOF, splitting the words and storing in an array 
         while ((line = br.readLine()) != null) {
-            lineList.add(line);
+            if (line.length() > 0) {
+                String[] words = line.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+                //Add individual words into a list of words
+                for (int i = 0; i < words.length; i++) {
+                    wordList.add(words[i]);
+                }
+            }
         }
 
-        //Extract individual words in each line and store in an array
+        //Iterate through individual words in the list 
+        for (int i = 0; i < wordList.size(); i++) {
+            count = 1;
+            for (int j = i + 1; j < wordList.size(); j++) {
+                if (wordList.get(i) == wordList.get(j)) {
+                    count++; 
+                }
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                String hfWord = wordList.get(i);
+                System.out.println("The word that appeared in highest frequency is: " + hfWord);
+            }
+        } 
 
+        //Store words as keys and wordcount as values in a hashmap
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : wordList) {
+            if (!wordCount.containsKey(word)){
+                wordCount.put(word, 0);
+            } else {
+                int counter = wordCount.get(word);
+                wordCount.put(word, counter++);
+            }
+        }
 
+    //     Map<String, Integer> wordcount = new HashMap<>();
+    //    //after assigning keys and values
+    //     wordcount.entrySet()
+    //             .stream()
+    //             .sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
+    //             .limit(10)
+    //             .collect(Collectors.toMap(
+    //                     Map.Entry::getKey,
+    //                     Map.Entry::getValue,
+    //                     (e1, e2) -> e1,
+    //                     LinkedHashMap::new
+    //             )).forEach((s, integer) -> System.out.println(String.format("%s : %s", s, integer)));
         
-    
     }
 } 
+
--- task01/src/Main.java
+++ task01/src/Main.java
@@ -1,9 +1,10 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Paths;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException, FileNotFoundException{
         
         String csvFile = args[0];
         String csvPath = Paths.get("./", csvFile).toString();
